d96b4b5b25dd5e6019e0187cc7d4ca9570f44c95
Johnny Corbino Delgado <johnnycorbino@gmail.com>
2017-08-17 11:16:31 -0700

    Updated comments

--- examples_MATLAB/elliptic1D.m
+++ examples_MATLAB/elliptic1D.m
@@ -5,14 +5,14 @@ close all
 
 addpath('../mole_MATLAB')
 
-west = 0; % Domain's limits
+west = 0;  % Domain's limits
 east = 1;
 
-k = 6; % Operator's order of accuracy
-m = 2*k+1; % Minimum number of cells to attain the desired accuracy
-dx = (east-west)/m; % Step size
+k = 6;  % Operator's order of accuracy
+m = 2*k+1;  % Minimum number of cells to attain the desired accuracy
+dx = (east-west)/m;  % Step length
 
-L = lap(k, m, dx); % 1D Mimetic laplacian operator
+L = lap(k, m, dx);  % 1D Mimetic laplacian operator
 
 % Impose Robin BC on laplacian operator
 a = 1;
@@ -20,15 +20,16 @@ b = 1;
 L = L + robinBC(k, m, dx, a, b);
 
 % 1D Staggered grid
-grid = [west west+dx/2: dx :east-dx/2 east];
+grid = [west west+dx/2 : dx : east-dx/2 east];
 
 % RHS
 U = exp(grid)';
-U(1) = 0; % West BC
-U(end) = 2*exp(1); % East BC
+U(1) = 0;  % West BC
+U(end) = 2*exp(1);  % East BC
 
-U = L\U; % Solve a linear system of equations
+U = L\U;  % Solve a linear system of equations
 
+% Plot result
 plot(grid, U, 'o-')
 title('Poisson''s equation with Robin BC')
 xlabel('x')

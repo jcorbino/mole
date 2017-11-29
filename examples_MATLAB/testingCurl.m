ef9ac2f8ee76a6884031872ca0b08eae23ed35d8
Johnny Corbino Delgado <johnnycorbino@gmail.com>
2017-11-29 15:59:53 -0800

    Basic example of how to obtain the curl
    In this basic example we apply the 2D mimetic divergence operator to an auxiliary vector field (F*) to obtain its curl.

---
+++ examples_MATLAB/testingCurl.m
@@ -0,0 +1,70 @@
+%
+% MOLE's Curl
+%
+
+clc
+close all
+
+order =   2;
+west  = -10;
+east  =  10;
+south = -10;
+north =  10;
+m = 20;
+n = 20;
+dx = (east-west)/m;
+dy = (north-south)/n;
+
+xaxis = west : (dx/2) : east;
+yaxis = south : (dy/2) : north;
+[X, Y] = meshgrid(xaxis(1:2:end), yaxis(1:2:end));
+
+A1 = zeros(2*m*n+m+n, 1);
+A2 = zeros(2*m*n+m+n, 1);
+
+% Vector field
+U = P(X, Y);
+V = Q(X, Y);
+
+k = 1;
+for j = 2 : 2 : 2*n+1
+    for i = 1 : 2 : 2*m+1
+        A1(k) = P(xaxis(i), yaxis(j));
+        A2(k) = Q(xaxis(i), yaxis(j));
+        k = k + 1;
+    end
+end
+
+for j = 1 : 2 : 2*n+1
+    for i = 2 : 2 : 2*m+1
+        A1(k) =  Q(xaxis(i), yaxis(j));
+        A2(k) = -P(xaxis(i), yaxis(j));
+        k = k + 1;   
+    end
+end
+
+addpath('../mole/mole_MATLAB')
+curlMOLE = div2D(order, m, dx, n, dy)*A2;
+curlMOLE = reshape(curlMOLE, m+2, n+2);
+curlMOLE = curlMOLE(2:end-1, 2:end-1);
+
+quiver3(X(2:end, 2:end), Y(2:end, 2:end), zeros(m, n),...
+        U(2:end, 2:end), V(2:end, 2:end), curlMOLE,... 
+        'AutoScale', 'off');
+title('Quiver plot');
+set(gca, 'XDir', 'reverse')
+set(gca, 'YDir', 'reverse')
+xlabel('x')
+ylabel('y')
+zlabel('z')
+set(gcf, 'color', 'w')
+view(-180, 90)
+
+% Modified vector field (F*)
+function U = P(~, Y)
+  U = -Y;
+end
+
+function V = Q(X, ~)
+  V = X;
+end

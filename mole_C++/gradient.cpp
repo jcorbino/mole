d8f3d04052d17f39d8dae58e1de1f07d138a0a3e
Johnny Corbino Delgado <johnnycorbino@gmail.com>
2017-12-10 15:01:05 -0800

    Update gradient.cpp

--- mole_C++/gradient.cpp
+++ mole_C++/gradient.cpp
@@ -238,7 +238,7 @@ Gradient::Gradient(u16 k, u32 m, u32 n, double dx, double dy)
     sp_mat G1 = Utils::spkron(In, Gx);
     sp_mat G2 = Utils::spkron(Gy, Im);
 
-    *this = spjoin_cols(G1, G2);
+    *this = Utils::spjoin_cols(G1, G2);
     // Dimensions = 2*m*n+m+n, (m+2)*(n+2)
     /*if (m != n)
         *this = join_cols(G1, G2);
@@ -272,7 +272,7 @@ Gradient::Gradient(u16 k, u32 m, u32 n, u32 o, double dx, double dy, double dz)
     sp_mat G2 = Utils::spkron(Utils::spkron(Io, Gy), Im);
     sp_mat G3 = Utils::spkron(Utils::spkron(Gz, In), Im);
 
-    *this = spjoin_cols(spjoin_cols(G1, G2), G3);
+    *this = Utils::spjoin_cols(Utils::spjoin_cols(G1, G2), G3);
     // Dimensions = HUGE
     /*if ((m != n) || (n != o))
         *this = join_cols(join_cols(G1, G2), G3);

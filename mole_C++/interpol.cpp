8cdb627de5c6950300c0d02b0fe669fe5679bda7
Johnny Corbino Delgado <johnnycorbino@gmail.com>
2017-12-10 16:44:23 -0800

    Update interpol.cpp

--- mole_C++/interpol.cpp
+++ mole_C++/interpol.cpp
@@ -32,16 +32,15 @@ Interpol::Interpol(u32 m, u32 n, double c1, double c2)
     sp_mat I1 = Utils::spkron(In, Ix);
     sp_mat I2 = Utils::spkron(Iy, Im);
 
-    *this = Utils::spjoin_cols(I1, I2);
     // Dimensions = 2*m*n+m+n, (m+2)*(n+2)
-    /*if (m != n)
-        *this = join_cols(I1, I2);
+    if (m != n)
+        *this = Utils::spjoin_cols(I1, I2);
     else {
         sp_mat A1(2, 1);
         sp_mat A2(2, 1);
         A1(0, 0) = A2(1, 0) = 1.0;
         *this = Utils::spkron(A1, I1) + Utils::spkron(A2, I2);
-    }*/
+    }
 }
 
 // 3-D Constructor
@@ -66,15 +65,14 @@ Interpol::Interpol(u32 m, u32 n, u32 o, double c1, double c2, double c3)
     sp_mat I2 = Utils::spkron(Utils::spkron(Io, Iy), Im);
     sp_mat I3 = Utils::spkron(Utils::spkron(Iz, In), Im);
 
-    *this = Utils::spjoin_cols(Utils::spjoin_cols(I1, I2), I3);
     // Dimensions = HUGE
-    /*if ((m != n) || (n != o))
-        *this = join_cols(join_cols(I1, I2), I3);
+    if ((m != n) || (n != o))
+        *this = Utils::spjoin_cols(Utils::spjoin_cols(I1, I2), I3);
     else {
         sp_mat A1(3, 1);
         sp_mat A2(3, 1);
         sp_mat A3(3, 1);
         A1(0, 0) = A2(1, 0) = A3(2, 0) = 1.0;
         *this = Utils::spkron(A1, I1) + Utils::spkron(A2, I2) + Utils::spkron(A3, I3);
-    }*/
+    }
 }

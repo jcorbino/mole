27743969cbaab259d435c56986cfe0aa343da82b
Johnny Corbino Delgado <johnnycorbino@gmail.com>
2018-04-24 22:41:31 -0700

    Update and rename curlF2D.m to curl2D.m

--- mole_MATLAB/curl2D.m
+++ mole_MATLAB/curl2D.m
@@ -1,4 +1,4 @@
-function C = curlF2D(k, m, dx, n, dy, west, east, north, south, U, V)
+function C = curl2D(k, m, dx, n, dy, west, east, north, south, U, V)
 % Returns a two-dimensional mimetic curl operator
 %
 % Parameters:
@@ -15,15 +15,14 @@ function C = curlF2D(k, m, dx, n, dy, west, east, north, south, U, V)
 %                    V(X,Y) must be defined as function handle
 
 
-% Argument validation
-    assert( isa(U, 'function_handle') && isa(V, 'function_handle'), ...
-        'U and V must be defined as function handle');
+% Arguments validation
+    assert(isa(U, 'function_handle') && isa(V, 'function_handle'), ...
+           'U and V must be defined as function handle');
+    assert(nargin(U) == 2 && nargin(V) == 2, ...
+           'U and V must be defined as 2D vector functions');
+    assert(west < east && south < north , 'west < east, south < north');
 
-    assert ( nargin(U) == 2 && nargin(V) == 2, ...
-        'U and V must be defined as 2D vector functions');
-    assert ( west < east && south < north , 'west < east, south < north' );
-
-% Variable initialization
+% Variables initialization
     F = zeros(2*m*n+m+n, 1);
     xaxis = west : (dx/2) : east;
     yaxis = south : (dy/2) : north;
@@ -32,19 +31,19 @@ function C = curlF2D(k, m, dx, n, dy, west, east, north, south, U, V)
     f = 1;
     for j = 2 : 2 : 2*n+1
         for i = 1 : 2 : 2*m+1
-            F(f) = V(xaxis(i), yaxis(j)); % F in the x direction (V_x)
+            F(f) = V(xaxis(i), yaxis(j));  % F in the x direction (V_x)
             f = f + 1;
         end
     end
 
     for j = 1 : 2 : 2*n+1
         for i = 2 : 2 : 2*m+1
-            F(f) = -U(xaxis(i), yaxis(j)); % F in the y direction (-U_y)
+            F(f) = -U(xaxis(i), yaxis(j));  % F in the y direction (-U_y)
             f = f + 1;
         end
     end
 
-    C = div2D(k, m, dx, n, dy) * F;  % mimetic curl as C = Div2D * F
-    C = reshape(C, m+2, n+2);        % reshaping C as matrix
-    C = C(2:end-1, 2:end-1);         % removing boundaries
+    C = div2D(k, m, dx, n, dy)*F;  % Mimetic curl as C = Div2D*F
+    C = reshape(C, m+2, n+2);      % Reshaping C as matrix
+    C = C(2:end-1, 2:end-1);       % Removing boundaries
 end

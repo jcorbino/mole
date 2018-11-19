a77d565e8bb174b9f92c16b9c37e49c34d926d63
Johnny Corbino Delgado <johnnycorbino@gmail.com>
2018-11-19 17:32:39 +0100

    Updated comment

--- mole_C++/divergence.cpp
+++ mole_C++/divergence.cpp
@@ -144,7 +144,7 @@ Divergence::Divergence(u16 k, u32 m, u32 n, u32 o, double dx, double dy, double 
     sp_mat D2 = Utils::spkron(Utils::spkron(Io, Dy), Im);
     sp_mat D3 = Utils::spkron(Utils::spkron(Dz, In), Im);
 
-    // Dimensions = HUGE
+    // Dimensions = (m+2)*(n+2)*(o+2), 3*m*n*o+m*n+m*o+n*o
     if ((m != n) || (n != o))
         *this = Utils::spjoin_rows(Utils::spjoin_rows(D1, D2), D3);
     else {

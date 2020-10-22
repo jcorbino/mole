bad9fca565eba2bce91052ff5556e23299897e24
Johnny Corbino <johnnycorbino@gmail.com>
2020-10-21 19:56:28 -0700

    added basic interpolator

--- mole_MATLAB/I.m
+++ mole_MATLAB/I.m
@@ -1,3 +1,21 @@
-function I = I(M)
-    I = M;
+function I = I(M, m, n, type)
+    if type == 'Gn'
+        I = sparse((m+1)*n, size(M, 2));
+        offset = m-1;
+        j = 1;
+        for i = 0 : n-1
+            I(j:j+offset, :) = M(j-i:j-i+offset, :);
+            I(j+offset+1, :) = I(j+offset, :);
+            j = j+offset+2;
+        end
+    else
+        I = sparse((n+1)*m, size(M, 2));
+        offset = m-1;
+        j = 1;
+        for i = 0 : n-1
+            I(j:j+offset, :) = M(j+i:j+i+offset, :);
+            j = j+offset+1;
+        end
+        I(j:j+offset, :) = I(j-m:j-1, :);
+    end
 end

401a71fb8f14b06b49cdf061a17392e670b12ec6
Johnny Corbino <johnnycorbino@gmail.com>
2020-10-24 18:46:57 -0700

    updated new grad2D based on metrics

--- examples_MATLAB/test_nodal2DCurv_3.m
+++ examples_MATLAB/test_nodal2DCurv_3.m
@@ -10,7 +10,6 @@ n = 40; % Number of nodes along y-axis
 
 [X, Y] = genCurvGrid(n, m);
 % [X, Y] = meshgrid(1:m, 1:n);
-[Xl, Yl] = meshgrid(1:m, 1:n); % Logical grids
 
 % Plot the physical grid
 mesh(X, Y, zeros(n, m), 'Marker', '.', 'MarkerSize', 10)
@@ -22,66 +21,12 @@ C = X.^2+Y.^2; % Given scalar field (on a nodal grid)
 % Staggered logical grid
 [Xs, Ys] = meshgrid([1 1.5 : 1 : m-0.5 m], [1 1.5 : 1 : n-0.5 n]);
 % Interpolate scalar field from nodal logical to staggered logical
-Cs = interp2(Xl, Yl, C, Xs, Ys);
+Cs = interp2(C, Xs, Ys);
 % Reshape the field so it can be multiplied by the operator later on
 C_ = reshape(Cs', [], 1);
 
-% Get the determinant of the jacobian and the metrics
-[J, Xe, Xn, Ye, Yn] = jacobian2D(k, X, Y);
-
-% Make them surfaces so they can be interpolated
-J = reshape(J, m, n)';
-Xe = reshape(Xe, m, n)';
-Xn = reshape(Xn, m, n)';
-Ye = reshape(Ye, m, n)';
-Yn = reshape(Yn, m, n)';
-
-% Interpolate the metrics on the logical grid for positions u and v
-Ju = interp2(Xl, Yl, J, (Xl(1:end-1, :)+Xl(2:end, :))/2,...
-                                        (Yl(1:end-1, :)+Yl(2:end, :))/2);
-Jv = interp2(Xl, Yl, J, (Xl(:, 1:end-1)+Xl(:, 2:end))/2,...
-                                        (Yl(:, 1:end-1)+Yl(:, 2:end))/2);
-Xeu = interp2(Xl, Yl, Xe, (Xl(1:end-1, :)+Xl(2:end, :))/2,...
-                                        (Yl(1:end-1, :)+Yl(2:end, :))/2);
-Xev = interp2(Xl, Yl, Xe, (Xl(:, 1:end-1)+Xl(:, 2:end))/2,...
-                                        (Yl(:, 1:end-1)+Yl(:, 2:end))/2);
-Xnu = interp2(Xl, Yl, Xn, (Xl(1:end-1, :)+Xl(2:end, :))/2,...
-                                        (Yl(1:end-1, :)+Yl(2:end, :))/2);
-Xnv = interp2(Xl, Yl, Xn, (Xl(:, 1:end-1)+Xl(:, 2:end))/2,...
-                                        (Yl(:, 1:end-1)+Yl(:, 2:end))/2);
-Yeu = interp2(Xl, Yl, Ye, (Xl(1:end-1, :)+Xl(2:end, :))/2,...
-                                        (Yl(1:end-1, :)+Yl(2:end, :))/2);
-Yev = interp2(Xl, Yl, Ye, (Xl(:, 1:end-1)+Xl(:, 2:end))/2,...
-                                        (Yl(:, 1:end-1)+Yl(:, 2:end))/2);
-Ynu = interp2(Xl, Yl, Yn, (Xl(1:end-1, :)+Xl(2:end, :))/2,...
-                                        (Yl(1:end-1, :)+Yl(2:end, :))/2);
-Ynv = interp2(Xl, Yl, Yn, (Xl(:, 1:end-1)+Xl(:, 2:end))/2,...
-                                        (Yl(:, 1:end-1)+Yl(:, 2:end))/2);
-
-% Convert metrics to diagonal matrices so they can be multiplied by the logical
-% operators
-Ju = spdiags(1./reshape(Ju', [], 1), 0, numel(Ju), numel(Ju));
-Jv = spdiags(1./reshape(Jv', [], 1), 0, numel(Jv), numel(Jv));
-Xeu = spdiags(reshape(Xeu', [], 1), 0, numel(Xeu), numel(Xeu));
-Xev = spdiags(reshape(Xev', [], 1), 0, numel(Xev), numel(Xev));
-Xnu = spdiags(reshape(Xnu', [], 1), 0, numel(Xnu), numel(Xnu));
-Xnv = spdiags(reshape(Xnv', [], 1), 0, numel(Xnv), numel(Xnv));
-Yeu = spdiags(reshape(Yeu', [], 1), 0, numel(Yeu), numel(Yeu));
-Yev = spdiags(reshape(Yev', [], 1), 0, numel(Yev), numel(Yev));
-Ynu = spdiags(reshape(Ynu', [], 1), 0, numel(Ynu), numel(Ynu));
-Ynv = spdiags(reshape(Ynv', [], 1), 0, numel(Ynv), numel(Ynv));
-
-% Construct 2D uniform mimetic gradient operator (d/de, d/dn)
-G = grad2D(k, m-1, 1, n-1, 1);
-Ge = G(1:m*(n-1), :);
-Gn = G(m*(n-1)+1:end, :);
-
-% Apply transformation
-Gx = Ju*(Ynu*Ge-Yeu*I(Gn, m-1, n-1, 'Gn'));
-Gy = Jv*(-Xnv*I(Ge, m-1, n-1, 'Ge')+Xev*Gn);
-
-% Final 2D curvilinear mimetic gradient operator (d/dx, d/dy)
-G = [Gx; Gy];
+% Get 2D curvilinear mimetic gradient
+G = grad2DCurv(k, X, Y);
 
 % Apply the operator to the field
 TMP = G*C_;
@@ -104,8 +49,7 @@ title('C')
 axis equal
 view([0 90])
 subplot(3, 1, 2)
-surf((X(1:end-1, :)+X(2:end, :))/2, (Y(1:end-1, :)+Y(2:end, :))/2, Gx,...
-    'EdgeColor', 'none');
+surf((X(1:end-1, :)+X(2:end, :))/2, (Y(1:end-1, :)+Y(2:end, :))/2, Gx, 'EdgeColor', 'none');
 colorbar
 xlabel('x')
 ylabel('y')
@@ -113,8 +57,7 @@ title('U')
 axis equal
 view([0 90])
 subplot(3, 1, 3)
-surf((X(:, 1:end-1)+X(:, 2:end))/2, (Y(:, 1:end-1)+Y(:, 2:end))/2, Gy,...
-    'EdgeColor', 'none');
+surf((X(:, 1:end-1)+X(:, 2:end))/2, (Y(:, 1:end-1)+Y(:, 2:end))/2, Gy, 'EdgeColor', 'none');
 colorbar
 xlabel('x')
 ylabel('y')

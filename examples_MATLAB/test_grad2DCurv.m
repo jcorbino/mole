005bb466cfbcbb76f17d9994a8bf86999995bde5
Johnny Corbino <johnnycorbino@gmail.com>
2020-10-24 19:42:18 -0700

    organized a bit

--- examples_MATLAB/test_grad2DCurv.m
+++ examples_MATLAB/test_grad2DCurv.m
@@ -1,77 +1,41 @@
-% Tests the 2D curvilinear gradient
 clc
 close all
 
 addpath('../mole_MATLAB')
 
 % Parameters
-k = 2;
-m = 40;
-n = 40;
+k = 2;  % Order of accuracy
+m = 40; % Number of nodes along x-axis
+n = 40; % Number of nodes along y-axis
 
 [X, Y] = genCurvGrid(n, m);
-%[X, Y] = meshgrid(1:m, 1:n);
+% [X, Y] = meshgrid(1:m, 1:n);
 
+% Plot the physical grid
 mesh(X, Y, zeros(n, m), 'Marker', '.', 'MarkerSize', 10)
 view([0 90])
 axis equal
 set(gcf, 'Color', 'w')
-hold on
 
-[n, m] = size(X);
-n = n-1;
-m = m-1;
+C = X.^2+Y.^2; % Given scalar field (on a nodal grid)
+% Staggered logical grid
+[Xs, Ys] = meshgrid([1 1.5 : 1 : m-0.5 m], [1 1.5 : 1 : n-0.5 n]);
+% Interpolate scalar field from nodal logical to staggered logical
+Cs = interp2(C, Xs, Ys);
+% Reshape the field so it can be multiplied by the operator later on
+C_ = reshape(Cs', [], 1);
 
-Ux = (X(1:end-1, :)+X(2:end, :))/2;
-Uy = (Y(1:end-1, :)+Y(2:end, :))/2;
-scatter3(Ux(:), Uy(:), zeros(n*(m+1), 1), '+', 'MarkerEdgeColor', 'k')
-
-Vx = (X(:, 1:end-1)+X(:, 2:end))/2;
-Vy = (Y(:, 1:end-1)+Y(:, 2:end))/2;
-scatter3(Vx(:), Vy(:), zeros((n+1)*m, 1), '*', 'MarkerEdgeColor', 'k')
-
-Cx = (Vx(1:end-1, :)+Vx(2:end, :))/2;
-Cy = (Uy(:, 1:end-1)+Uy(:, 2:end))/2;
-scatter3(Cx(:), Cy(:), zeros(n*m, 1), '.', 'MarkerEdgeColor', 'r')
-
-% West-East sides
-Cx = [Ux(:, 1) Cx];
-Cy = [Uy(:, 1) Cy];
-Cx = [Cx Ux(:, end)];
-Cy = [Cy Uy(:, end)];
-
-% South-North sides
-Cx = [[0 Vx(1, :) 0]; Cx];
-Cy = [[0 Vy(1, :) 0]; Cy];
-Cx = [Cx; [0 Vx(end, :) 0]];
-Cy = [Cy; [0 Vy(end, :) 0]];
-
-% Corners
-Cx(1, 1) = X(1, 1);
-Cy(1, 1) = Y(1, 1);
-Cx(1, end) = X(1, end);
-Cy(1, end) = Y(1, end);
-Cx(end, 1) = X(end, 1);
-Cy(end, 1) = Y(end, 1);
-Cx(end, end) = X(end, end);
-Cy(end, end) = Y(end, end);
-
-scatter3(Cx(:), Cy(:), zeros((m+2)*(n+2), 1), 'o', 'MarkerEdgeColor', 'r')
-legend('Nodal points', 'u', 'v', 'Centers', 'All centers')
-hold off
-
-tic
+% Get 2D curvilinear mimetic gradient
 G = grad2DCurv(k, X, Y);
-toc
 
-Cgiven = Cx.^2+Cy.^2;
+% Apply the operator to the field
+TMP = G*C_;
+Gx = TMP(1:m*(n-1));
+Gy = TMP(m*(n-1)+1:end);
 
-UV = G*reshape(Cgiven', [], 1);
-
-U = UV(1:n*(m+1));
-U = reshape(U, m+1, n)';
-V = UV(n*(m+1)+1:end);
-V = reshape(V, m, n+1)';
+% Reshape for visualization
+Gx = reshape(Gx, m, n-1)';
+Gy = reshape(Gy, m-1, n)';
 
 % Plot results
 figure
@@ -85,7 +49,7 @@ title('C')
 axis equal
 view([0 90])
 subplot(3, 1, 2)
-surf((X(1:end-1, :)+X(2:end, :))/2, (Y(1:end-1, :)+Y(2:end, :))/2, U, 'EdgeColor', 'none');
+surf((X(1:end-1, :)+X(2:end, :))/2, (Y(1:end-1, :)+Y(2:end, :))/2, Gx, 'EdgeColor', 'none');
 colorbar
 xlabel('x')
 ylabel('y')
@@ -93,7 +57,7 @@ title('U')
 axis equal
 view([0 90])
 subplot(3, 1, 3)
-surf((X(:, 1:end-1)+X(:, 2:end))/2, (Y(:, 1:end-1)+Y(:, 2:end))/2, V, 'EdgeColor', 'none');
+surf((X(:, 1:end-1)+X(:, 2:end))/2, (Y(:, 1:end-1)+Y(:, 2:end))/2, Gy, 'EdgeColor', 'none');
 colorbar
 xlabel('x')
 ylabel('y')

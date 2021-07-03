c817d68f572980b27d7153243a5f398485ad4330
Johnny Corbino Delgado <johnnycorbino@gmail.com>
2021-07-02 18:54:36 -0700

    Create amean.m

---
+++ mole_MATLAB/amean.m
@@ -0,0 +1,9 @@
+function Y = amean(X)
+% Returns the arithmetic mean for every two pairs of a column vector
+% And, Y(1) = X(1), Y(end) = X(end)
+%
+% Parameters:
+%                X : Column vector
+
+    Y = [X(1); (X(1:end-1)+X(2:end))/2; X(end)];
+end

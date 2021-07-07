05e4d0b3812b048dac2e77419b3a724afe45c0b3
Johnny Corbino Delgado <johnnycorbino@gmail.com>
2021-07-06 21:54:56 -0700

    Update RK4.m
    This example is simply to illustrate the implementation of a traditional Runge-Kutta scheme. It doesn't involve mimetic operators.

--- examples_MATLAB/RK4.m
+++ examples_MATLAB/RK4.m
@@ -8,8 +8,8 @@ f = @(t, y) sin(t)^2*y;                     % f(t, y)
 
 for i = 1 : length(t) - 1                   % Stages
     k1 = f(t(i),       y(i));
-    k2 = f(t(i) + h/2, y(i) + h*k1/2);
-    k3 = f(t(i) + h/2, y(i) + h*k2/2);
+    k2 = f(t(i) + h/2, y(i) + h/2*k1);
+    k3 = f(t(i) + h/2, y(i) + h/2*k2);
     k4 = f(t(i) + h,   y(i) + h*k3);
     
     y(i + 1) = y(i) + h/6*(k1 + 2*k2 + 2*k3 + k4);  % y(i + 1)

7e7d2829ac184ffabcf28ad895b7bff1c242635f
Johnny Corbino Delgado <johnnycorbino@gmail.com>
2021-07-18 07:47:56 -0700

    Update utils.h

--- mole_C++/utils.h
+++ mole_C++/utils.h
@@ -9,9 +9,9 @@ class Utils
 {
 
 public:
-    static sp_mat spkron(sp_mat A, sp_mat B);
-    static sp_mat spjoin_rows(sp_mat A, sp_mat B);
-    static sp_mat spjoin_cols(sp_mat A, sp_mat B);
+    static sp_mat spkron(const sp_mat &A, const sp_mat &B);
+    static sp_mat spjoin_rows(const sp_mat &A, const sp_mat &B);
+    static sp_mat spjoin_cols(const sp_mat &A, const sp_mat &B);
 };
 
 #endif // UTILS_H

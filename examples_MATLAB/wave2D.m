f147077ed9569c8136b9ecd6a704f996a2b1c8c8
Johnny Corbino Delgado <johnnycorbino@gmail.com>
2021-07-22 19:27:49 -0700

    Update wave2D.m

--- examples_MATLAB/wave2D.m
+++ examples_MATLAB/wave2D.m
@@ -74,6 +74,7 @@ for t = 1 : TIME/dt
     title(['Elastic membrane with position Verlet \newlineTime = ' num2str(dt*t, '%1.2f')])
     xlabel('x')
     ylabel('y')
+    zlabel('z')
     colorbar
     caxis([-1, 1])
     axis([0 1 0 1 -1 1])

cbb0382998fe08e6d3b552066648f45789959bb1
Johnny Corbino Delgado <johnnycorbino@gmail.com>
2023-02-09 11:49:48 -0800

    Update schrodinger2D.m

--- examples_MATLAB/schrodinger2D.m
+++ examples_MATLAB/schrodinger2D.m
@@ -6,12 +6,11 @@ addpath('../mole_MATLAB')
 
 % Parameters
 Lxy = 1;              % Length of box in x and y
-k = 2;                % Order of accuracy        
-m = 50;               % Grid points in x     
-n = 50;               % Grid points in y 
-t = 1;                % Time
-nx = 2;               % Energy level in x 
-ny = 2;               % Energy level in y  
+k = 2;                % Order of accuracy
+m = 50;               % Grid points in x
+n = 50;               % Grid points in y
+nx = 2;               % Energy level in x
+ny = 2;               % Energy level in y
 c = 1;                % Wave propagation speed
 kx = @(nx) nx*pi/Lxy; % Wave vector in x
 ky = @(ny) ny*pi/Lxy; % Wave vector in y
@@ -21,7 +20,7 @@ dt = dx/(2*c);        % Time step
 
 % Spatial discretization
 xGrid = [0 dx/2:dx:Lxy-dx/2 Lxy]; % Staggered grid x
-yGrid = [0 dy/2:dy:Lxy-dy/2 Lxy]; % Staggered grid y 
+yGrid = [0 dy/2:dy:Lxy-dy/2 Lxy]; % Staggered grid y
 [Y, X] = meshgrid(yGrid, xGrid);  % Grid
 
 % Initial condition
@@ -32,13 +31,13 @@ vold = IC_v(X, Y);
 
 % Mimetic Laplacian operator and interpolator
 L = lap2D(k, m, dx, n, dy);
-L = L + robinBC2D(k, m, dx, n, dy, 1, 0); 
+L = L + robinBC2D(k, m, dx, n, dy, 1, 0);
 I = interpol2D(m, n, 0.5, 0.5);
 I2 = interpolD2D(m, n, 0.5, 0.5);
 
 % Premultiplying for convenience 
-I = dt*I; 
-I2 = 0.5*dt*I2; 
+I = dt*I;
+I2 = 0.5*dt*I2;
 
 % Hamiltonian function
 H = @(x) 0.5*L*x;
@@ -50,9 +49,9 @@ v_old = IC_v(X, Y);
 
 for i = 0:100
     
-    % Position Verlet algorithm 
-    Psi_old = Psi_old + I2*v_old; 
-    v_new = v_old + I*H(Psi_old); 
+    % Position Verlet algorithm
+    Psi_old = Psi_old + I2*v_old;
+    v_new = v_old + I*H(Psi_old);
     Psi_new = Psi_old + I2*v_new;
     Psi_re = reshape(Psi_new, m+2, n+2);
     

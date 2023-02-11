5145923ef6296655124f3ce13e004ddc7c4af859
Johnny Corbino Delgado <johnnycorbino@gmail.com>
2023-02-11 09:20:17 -0800

    Update schrodinger2D.m

--- examples_MATLAB/schrodinger2D.m
+++ examples_MATLAB/schrodinger2D.m
@@ -11,23 +11,16 @@ m = 50;               % Grid points in x
 n = 50;               % Grid points in y
 nx = 2;               % Energy level in x
 ny = 2;               % Energy level in y
-c = 1;                % Wave propagation speed
 kx = @(nx) nx*pi/Lxy; % Wave vector in x
 ky = @(ny) ny*pi/Lxy; % Wave vector in y
 dx = Lxy / m;         % Step in x
 dy = Lxy / n;         % Step in y
-dt = dx/(2*c);        % Time step
+dt = dx;              % Time step
 
 % Spatial discretization
 xGrid = [0 dx/2:dx:Lxy-dx/2 Lxy]; % Staggered grid x
 yGrid = [0 dy/2:dy:Lxy-dy/2 Lxy]; % Staggered grid y
 [Y, X] = meshgrid(yGrid, xGrid);  % Grid
-
-% Initial condition
-A = 2/Lxy;
-IC_Psi = @(x, y, nx, ny) A.*sin(kx(nx)*x).*sin(ky(ny)*y);
-IC_v = @(x, y) zeros(2*m*n+m+n, 1);
-vold = IC_v(X, Y);
 
 % Mimetic Laplacian operator and interpolator
 L = lap2D(k, m, dx, n, dy);
@@ -35,7 +28,7 @@ L = L + robinBC2D(k, m, dx, n, dy, 1, 0);
 I = interpol2D(m, n, 0.5, 0.5);
 I2 = interpolD2D(m, n, 0.5, 0.5);
 
-% Premultiplying for convenience 
+% Premultiplying
 I = dt*I;
 I2 = 0.5*dt*I2;
 
@@ -43,11 +36,12 @@ I2 = 0.5*dt*I2;
 H = @(x) 0.5*L*x;
 
 % Initialization
-Psi_old = IC_Psi(X, Y, nx, ny);
+A = 2/Lxy;
+Psi_old = A*sin(kx(nx)*X).*sin(ky(ny)*Y);
 Psi_old = Psi_old(:);
-v_old = IC_v(X, Y);
+v_old = zeros(2*m*n+m+n, 1);
 
-for i = 0:100
+for i = 0:105
     
     % Position Verlet algorithm
     Psi_old = Psi_old + I2*v_old;
@@ -57,10 +51,10 @@ for i = 0:100
     
     % Plotting
     surf(X, Y, reshape(Psi_new, m+2, n+2))
-    zlim([-A A]);
     xlabel('x')
     ylabel('y')
     zlabel('\psi')
+    zlim([-A A]);
     title(['nx = ',num2str(nx),', ny = ',num2str(ny),', t = ',num2str(i)]);
     drawnow
     

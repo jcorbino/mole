4e4091cc45a4b25a7c0253181551b277c26eab3d
Johnny Corbino Delgado, Ph.D. <johnnycorbino@gmail.com>
2026-02-01 10:50:59 -0800

    Update maxwell2D_UPML_vectorized_sinusoidal.m

--- examples_MATLAB/maxwell2D_UPML_vectorized_sinusoidal.m
+++ examples_MATLAB/maxwell2D_UPML_vectorized_sinusoidal.m
@@ -2,10 +2,14 @@
 clear; clc; close all;
 addpath("../mole_MATLAB");
 
-% Parameters
+% Domain
 nt = 500;
-mx = 100;  my = 100;
-west = 0; east = 1;  south = 0; north = 1;
+mx = 100;
+my = 100;
+west = 0;
+east = 1;
+south = 0;
+north = 1;
 
 dx = (east-west)/mx;
 dy = (north-south)/my;
@@ -22,24 +26,25 @@ G = dt * grad2D(k,mx,dx,my,dy);
 D = dt * div2D(k,mx,dx,my,dy);
 
 % Fields
-E = zeros((mx+2)*(my+2),1);   % Sinusoidal source
-NBx = mx*(my+1);  NBy = (mx+1)*my;
+E = zeros((mx+2)*(my+2),1);
+NBx = mx*(my+1);
+NBy = (mx+1)*my;
 B = zeros(NBx+NBy,1);
 
 % Vectorized UPML parameters
-pml = 30;          
-sigma_max = 100;   
-m = 4;             
+pml = 30;
+sigma_max = 100;
+m = 4;
 
-ix = (1:mx+2)'; 
+ix = (1:mx+2)';
 iy = (1:my+2)';
 
 sigma_x = zeros(mx+2,1);
 sigma_y = zeros(my+2,1);
 
-Lx = ix <= pml;                     
+Lx = ix <= pml;
 Rx = ix >= mx+3-pml;
-Ly = iy <= pml;                     
+Ly = iy <= pml;
 Ry = iy >= my+3-pml;
 
 sigma_x(Lx) = sigma_max*((pml - ix(Lx) + 1)/pml).^m;
@@ -48,18 +53,18 @@ sigma_x(Rx) = sigma_max*((ix(Rx) - (mx+2-pml))/pml).^m;
 sigma_y(Ly) = sigma_max*((pml - iy(Ly) + 1)/pml).^m;
 sigma_y(Ry) = sigma_max*((iy(Ry) - (my+2-pml))/pml).^m;
 
-SIGE = sigma_y + sigma_x.';   
-aE = exp(-SIGE(:)*dt);        
+SIGE = sigma_y + sigma_x.';
+aE = exp(-SIGE(:)*dt); % Damping profile for E
 
-sigmaBx = sigma_y(1:my+1) + sigma_x(2:mx+1).';   
-sigmaBy = sigma_y(2:my+1) + sigma_x(1:mx+1).';   
+sigmaBx = sigma_y(1:my+1) + sigma_x(2:mx+1).';
+sigmaBy = sigma_y(2:my+1) + sigma_x(1:mx+1).';
 
-aB = exp(-[sigmaBx(:); sigmaBy(:)] * dt);        
+aB = exp(-[sigmaBx(:); sigmaBy(:)] * dt); % Damping profile for B
 
 % Leapfrog start
 B = aB .* (B - 0.5 * G * E);
 
-% Plot
+% Fancy plotting
 figure('Color','w');
 hSurf = surf(xE,yE,reshape(E,my+2,mx+2));
 shading interp; colormap(hot);
@@ -70,14 +75,13 @@ zlim([-1 1]); clim([-1 1]);
 titleHandle = title('Sinusoidal pulse with UPML. Step: 0');
 
 % Sinusoidal source setup
-f = 10;   % Frequency
+f = 10; % Frequency
 ix0 = round((mx+2)/2);
 iy0 = round((my+2)/2);
 src_index = sub2ind([my+2, mx+2], iy0, ix0);
 
 % Time stepping
 for n = 1:nt
-
     % Magnetic update
     B = aB .* (B - G*E);
 

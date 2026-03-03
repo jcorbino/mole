cf0fae61db5daaafa3377f4c41b072ce893379ac
Johnny Corbino Delgado, Ph.D. <johnnycorbino@gmail.com>
2026-03-02 20:31:52 -0800

    Update backward_euler.m

--- examples_MATLAB/backward_euler.m
+++ examples_MATLAB/backward_euler.m
@@ -1,6 +1,10 @@
 % Solves ODE using backward Euler method
 
-h = .01;                                    % Step-size
+clc;
+clear;
+close all;
+
+h = .1;                                     % Step-size
 t = 0 : h : 5;                              % Calculates up to y(5)
 y = zeros(1, length(t));
 y(1) = 2;                                   % Initial condition
@@ -11,8 +15,15 @@ for i = 1 : length(t) - 1                   % Stages
     y(i+1) = fzero(@(y) y - h*f(t(i+1), y) - old_y, old_y); % Backward Euler
 end
 
-plot(t, y)
-title('Approximation to y(t) using backward Euler')
-xlabel('t')
-ylabel('y')
-grid on
+% Exact solution
+y_exact = 2*exp(t/2 - sin(2*t)/4);
+
+% Plot both
+plot(t, y, 'ro-');
+hold on;
+plot(t, y_exact, 'b--');
+title('IVP solved with backward Euler');
+xlabel('x');
+ylabel('y');
+legend('Approximation', 'Exact', 'Location', 'northwest');
+grid on;
